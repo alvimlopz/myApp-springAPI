78e45169ad416b3aefd4042e814035aecac18868
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-26 14:06:25 -0300

    Criando o Repository e fazendo a injeção de dependencia

--- src/main/java/com/alvimCode/myApp/controller/CourseController.java
+++ src/main/java/com/alvimCode/myApp/controller/CourseController.java
@@ -2,15 +2,33 @@ package com.alvimCode.myApp.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alvimCode.myApp.model.Course;
+import com.alvimCode.myApp.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/api/courses")
-public class CourseController {
 
-	public List<Object> list(){
-		return null;
+@Component
+@AllArgsConstructor //injeção de dependencia com lombok
+public class CourseController {
+	
+	private final CourseRepository courseRepository;
+	
+	//subistituindo a anotação de autowired
+	public CourseController(CourseRepository courseRepository) {
+		this.courseRepository = courseRepository;
+	}
+
+	@GetMapping
+	public List<Course> list(){
+		return courseRepository.findAll();
 	}
 	
 }
