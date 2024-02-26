fefe1f170324e61fdf20dd33cc86f359b0b55495
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-26 13:38:41 -0300

    Iniando a API spring para o app de cursos

---
+++ src/main/java/com/alvimCode/myApp/controller/CourseController.java
@@ -0,0 +1,16 @@
+package com.alvimCode.myApp.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+	public List<Object> list(){
+		return null;
+	}
+	
+}
